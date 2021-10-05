ee9826512ad8f1e5ac6a1b60867d31fbe12997bf
4uBaKiCh <91912638+4uBaKiCh@users.noreply.github.com>
2021-10-05 15:21:32 +0300

    Add files via upload

---
+++ Zero assignment/Zero_assignment.py
@@ -0,0 +1,19 @@
+f = open('Text.txt', "r", encoding="utf-8")
+text = f.read()
+f.close
+text = text.lower()
+import string
+spec_chars = string.punctuation + '\n\xa0«»\t1234567890'
+text = "".join([ch for ch in text if ch not in spec_chars])
+from nltk import word_tokenize
+text_tokens = word_tokenize(text)
+import nltk
+text = nltk.Text(text_tokens)
+from nltk.corpus import stopwords
+russian_stopwords = stopwords.words("russian")
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+% matplotlib inline
+text_raw = " ".join(text)
+wordcloud = WordCloud(stopwords = russian_stopwords).generate(text_raw)
+wordcloud.to_file('result.png')
